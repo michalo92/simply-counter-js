d3a8d8fde593463a459a02a1819878d4da5e4b98
Michal Pomorski <mipomorski@gmail.com>
2023-04-13 16:27:13 +0100

    Change js to foreach

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
-const decrease = document.getElementById("decrease");
-const reset = document.getElementById("reset");
-const increase = document.getElementById("increase");
 const counter = document.querySelector(".number");
+const btn = document.querySelectorAll(".button");
 
 let number = 0;
 counter.textContent = number;
 
-decrease.addEventListener("click", () => {
-  number--;
-  setNumber(number);
-});
-increase.addEventListener("click", () => {
-  number++;
-  setNumber(number);
-});
-reset.addEventListener("click", (e) => {
-  number = 0;
-  setNumber(number);
-});
-function setNumber(number) {
-  counter.textContent = number;
-}
+btn.forEach((el) =>
+  el.addEventListener("click", () => {
+    if (el.id === "decrease") {
+      number--;
+      counter.textContent = number;
+    }
+    if (el.id === "reset") {
+      counter.textContent = number = 0;
+    }
+    if (el.id === "increase") {
+      number++;
+      counter.textContent = number;
+    }
+  })
+);
