9b12c8a4f0475dfe0d6c73c05dfc10d07bb65164
Michal Pomorski <mipomorski@gmail.com>
2023-04-13 16:34:03 +0100

    change js v1.03

--- script.js
+++ script.js
@@ -4,18 +4,20 @@ const btn = document.querySelectorAll(".button");
 let number = 0;
 counter.textContent = number;
 
-btn.forEach((el) =>
-  el.addEventListener("click", () => {
-    if (el.id === "decrease") {
+btn.forEach((el) => {
+  if (el.id === "decrease") {
+    el.addEventListener("click", () => {
       number--;
       counter.textContent = number;
-    }
-    if (el.id === "reset") {
-      counter.textContent = number = 0;
-    }
-    if (el.id === "increase") {
+    });
+  }
+  if (el.id === "reset") {
+    el.addEventListener("click", () => (counter.textContent = number = 0));
+  }
+  if (el.id === "increase") {
+    el.addEventListener("click", () => {
       number++;
       counter.textContent = number;
-    }
-  })
-);
+    });
+  }
+});
